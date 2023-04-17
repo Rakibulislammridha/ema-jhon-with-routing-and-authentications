23aca6c8ec584d7800eaf45d36a9cc0f3218cbe8
Rakibulislammridha <dr.rakibulislammridha@gmail.com>
2023-04-18 01:00:48 +0600

    firebase install and set sign up route video-1

---
+++ src/__steps__.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * 1. create a firebase project
+ * 2. create a web app
+ * 3. npm install fire base
+ * 4. save firebase config and export default app
+ * 5. build > authentication > get started > enable sign in method
+ * 6. create sign in  and login route
+ *
+ *
+ */
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAHdpMWR0PBn4blQephmf4jwQx8llC5_Qc",
+  authDomain: "ema-jhon-with-firebase-a-c13cc.firebaseapp.com",
+  projectId: "ema-jhon-with-firebase-a-c13cc",
+  storageBucket: "ema-jhon-with-firebase-a-c13cc.appspot.com",
+  messagingSenderId: "602088992992",
+  appId: "1:602088992992:web:37589dc9699ea9ee62e085",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
