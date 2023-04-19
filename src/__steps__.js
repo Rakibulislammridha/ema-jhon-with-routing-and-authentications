859302a8fe474fa5b7cbc4e30903bdbd3056a092
Rakibulislammridha <dr.rakibulislammridha@gmail.com>
2023-04-19 17:55:17 +0600

    final submintion

--- src/__steps__.js
+++ src/__steps__.js
@@ -18,3 +18,22 @@
  * 5. set the provider
  *
  */
+
+/**
+ *
+ * Hosting
+ *
+ * 1. one time per pc (npm install -g firebase-tools  > 1.5 firebase login)
+ * 2. for each project one time (firebase init) in there many question is here answer those very carefully
+ * 2.1 when complete init  > proceed > firebase hosting (up and down arrow on keyboard to select it) > click space bar to select
+ * 2.2 existing project
+ * 2.3 select the project carefully
+ * 2.4 which project as public directory : dist
+ * 2.5 single page application : yes
+ * 2.6 continuous deployment : no
+ *
+ * 3. for every time deploy
+ * 3.1 npm run build
+ * 3.2 firebase deploy
+ *
+ */
